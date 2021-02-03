59b74cf8ed6617ff36ca5de4468b07a6bf836cc3
danteccarhuas <ccarhuas.dante@gmail.com>
2021-02-03 16:27:55 -0500

    cambios para proyecto citasreact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,75 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Header from "./componentes/Header";
+import AgregarCita from "./componentes/AgregarCita";
+import ListaCitas from "./componentes/ListaCitas";
 
 class App extends Component {
-  render(){
+  state = {
+    citas: [],
+  };
+
+  //Se ejecuta una vez haya cargado el componente
+  componentDidMount() {
+    const citasLS = localStorage.getItem("citas");
+    if(citasLS){
+      this.setState({
+        citas: JSON.parse(citasLS)
+      });
+    }
+  }
+
+  //Se ejecuta antes de que el componente se carga
+  componentWillMount() {
+    console.log("Yo me ejecuto antes");
+  }
+
+  //Se ejecuta cuando se cierra el componente
+  componentWillUnmount() {
+    console.log("Yo hasta que cierre el componente");
+  }
+
+  //Se ejecuta cuando cambia algo en componente
+  componentDidUpdate() {
+    localStorage.setItem("citas", JSON.stringify(this.state.citas));
+  }
+
+  crearCita = (nuevaCita) => {
+    //Agregar la nuevata cita al inicio el array de esta manera
+    //[nuevaCita, ...this.state.citas];
+    const citas = [...this.state.citas, nuevaCita];
+
+    this.setState({
+      citas,
+    });
+    console.log(citas);
+  };
+
+  borrarCita = (id) => {
+    console.log(id);
+    //Obtener copia del state
+    const citasActuales = [...this.state.citas];
+
+    //borrar el elemento del state
+    const citas = citasActuales.filter((cita) => cita.id !== id);
+
+    //actualizar el state
+    this.setState({
+      citas,
+    });
+  };
+
+  render() {
     return (
-      <div className="App">
-  
+      <div className="container">
+        <Header titulo={"Administrador de Pacientes de Veterinaria"} />
+        <div className="row">
+          <div className="col-md-6">
+            <AgregarCita crearCita={this.crearCita} />
+          </div>
+          <div className="col-md-6">
+            <ListaCitas citas={this.state.citas} borrarCita={this.borrarCita} />
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/componentes/AgregarCita.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import {v4 as uuid} from 'uuid';
+import PropTypes from 'prop-types';
+
+class AgregarCita extends Component {
+    
+    //Crear refs para leer los inputs
+    nombreMascotaRef = React.createRef();
+    propietarioRef = React.createRef();
+    fechaRef = React.createRef();
+    horaRef = React.createRef();
+    sintomasRef = React.createRef();
+    
+    state = { 
+        error: false
+     }
+
+    creareNuevaCita = (e) =>{
+        e.preventDefault();
+
+        const mascota = this.nombreMascotaRef.current.value,
+        propietario = this.propietarioRef.current.value,
+        fecha = this.fechaRef.current.value,
+        hora = this.horaRef.current.value,
+        sintomas = this.sintomasRef.current.value;
+
+        if(mascota === '' || propietario === '' || fecha === '' || hora === '' || sintomas === ''){
+            this.setState({
+                error: true
+            });
+        }else{
+            const nuevaCita = {
+                id: uuid(),
+                mascota,
+                propietario,
+                fecha,
+                hora,
+                sintomas
+            }
+    
+            //Se envia el objeto hacia el padre para actualizar el state
+            this.props.crearCita(nuevaCita);
+    
+            //Reiniciar el formulario
+            e.currentTarget.reset();
+
+            this.setState({
+                error: false
+            });
+        }
+    }
+
+    render() { 
+        const existeError = this.state.error;
+        return ( 
+            <div className="card mt-5">
+                <div className="card-body">
+                    <h2 className="card-title text-center mb-5">Agregar las citas aquí</h2>
+                    <form onSubmit={this.creareNuevaCita}>
+                        <div className="form-group row">
+                            <label className="col-sm-4 col-lg-2 col-form-label">Nombre Mascota</label>
+                            <div className="col-sm-8 col-lg-10">
+                                <input type="text" className="form-control" placeholder="Nombre Mascota" ref={this.nombreMascotaRef}/>
+                            </div>
+                        </div>
+                        <div className="form-group row">
+                            <label className="col-sm-4 col-lg-2 col-form-label">Nombre Dueño</label>
+                            <div className="col-sm-8 col-lg-10">
+                                <input type="text" className="form-control"  placeholder="Nombre Dueño de la Mascota" ref={this.propietarioRef}/>
+                            </div>
+                        </div>
+
+                        <div className="form-group row">
+                            <label className="col-sm-4 col-lg-2 col-form-label">Fecha</label>
+                            <div className="col-sm-8 col-lg-4  mb-4 mb-lg-0">
+                                <input type="date" className="form-control" ref={this.fechaRef}/>
+                            </div>                            
+
+                            <label className="col-sm-4 col-lg-2 col-form-label">Hora</label>
+                            <div className="col-sm-8 col-lg-4">
+                                <input type="time" className="form-control" ref={this.horaRef}/>
+                            </div>
+                        </div>
+
+                        <div className="form-group row">
+                            <label className="col-sm-4 col-lg-2 col-form-label">Sintomas</label>
+                            <div className="col-sm-8 col-lg-10">
+                                <textarea  className="form-control" ref={this.sintomasRef}></textarea>
+                            </div>
+                        </div>
+                        <div className="form-group row justify-content-end">
+                            <div className="col-sm-3">
+                                <button type="submit" className="btn btn-success w-100">Agregar</button>
+                            </div>
+                        </div>
+                    </form>
+                    { existeError ? <div className="alert alert-danger text-center">Todos los Campos son Obligatorios</div> : '' }
+                </div>
+            </div>
+         );
+    }
+}
+
+AgregarCita.propTypes = {
+    crearCita: PropTypes.func.isRequired
+}
+ 
+export default AgregarCita;
